b317db4c80b3dc07161a62823213255d452a5603
Daniel Lanier <lanier4@mntl-dhcp108.mntl.illinois.edu>
2017-03-14 11:21:02 -0500

    docker container test scripts

---
+++ notebooks/cloud_test_DCP.py
@@ -0,0 +1,128 @@
+
+# coding: utf-8
+
+# ### KnowEnG-Research/Data_Cleanup_Pipeline test all methods notebook (for script run on cloud)
+
+# In[ ]:
+
+SHOW_COMMAND_LINE_OUTPUT = False
+
+import os
+import sys
+
+dcp_src = '../../src/'
+sys.path.insert(1, dcp_src)
+import data_cleanup_toolbox as dc
+
+sys.path.insert(1, '../../keg_test_tools/src')
+import dcp_test
+
+import knpackage.toolbox as kn
+
+# If not running inside a docker container of the KnowEnG variety: Create this Directory
+output_data_dir = '../../test/run_dir'
+
+
+# In[ ]:
+
+#                   Data_Cleanup_Pipeline   Samples Clustering
+yaml_dir = '../../../Data_Cleanup_Pipeline/data/run_files'
+yaml_file = 'TEMPLATE_data_cleanup.yml'
+run_parameters = kn.get_run_parameters(yaml_dir, yaml_file)   
+
+run_parameters['pheno_data_dir'] = '../../keg_test_tools/data/samples_clustering/phenotypes_SC'
+run_parameters['spreadsheet_data_dir'] = '../../keg_test_tools/data/samples_clustering/spreadsheets_SC'
+run_parameters['pipeline_type'] = 'samples_clustering_pipeline'
+
+run_parameters['run_directory'] = dcp_src
+run_parameters['results_directory'] = kn.create_dir(dir_path=output_data_dir, dir_name='samples_clustering')
+
+run_parameters['SHOW_COMMAND_LINE_OUTPUT'] = SHOW_COMMAND_LINE_OUTPUT
+
+if SHOW_COMMAND_LINE_OUTPUT:
+    for p in list(run_parameters.keys()):
+        print(p, ':\t\t', run_parameters[p])
+
+test_result_df = dcp_test.test_data_cleanup(run_parameters)
+
+
+# In[ ]:
+
+#                   Data_Cleanup_Pipeline   Gene_Prioritization pearson
+yaml_dir = '../../../Data_Cleanup_Pipeline/data/run_files'
+yaml_file = 'TEMPLATE_data_cleanup.yml'
+run_parameters = kn.get_run_parameters(yaml_dir, yaml_file)   
+
+run_parameters['pheno_data_dir'] = '../../keg_test_tools/data/gene_prioritization/phenotypes_pearson'
+run_parameters['spreadsheet_data_dir'] = '../../keg_test_tools/data/gene_prioritization/spreadsheets_GP'
+run_parameters['pipeline_type'] = 'gene_prioritization_pipeline'
+run_parameters['correlation_measure'] = 'pearson'
+
+run_parameters['run_directory'] = dcp_src
+run_parameters['results_directory'] = kn.create_dir(dir_path=output_data_dir, dir_name='gene_prioritaization_t_test')
+
+run_parameters['SHOW_COMMAND_LINE_OUTPUT'] = SHOW_COMMAND_LINE_OUTPUT
+
+if SHOW_COMMAND_LINE_OUTPUT:
+    for p in list(run_parameters.keys()):
+        print(p, ':\t\t', run_parameters[p])
+
+test_result_df = dcp_test.test_data_cleanup(run_parameters)
+
+
+# In[ ]:
+
+#                   Data_Cleanup_Pipeline   Gene_Prioritization t_test
+yaml_dir = '../../../Data_Cleanup_Pipeline/data/run_files'
+yaml_file = 'TEMPLATE_data_cleanup.yml'
+run_parameters = kn.get_run_parameters(yaml_dir, yaml_file)   
+
+run_parameters['pheno_data_dir'] = '../../keg_test_tools/data/gene_prioritization/phenotypes_t_test'
+run_parameters['spreadsheet_data_dir'] = '../../keg_test_tools/data/gene_prioritization/spreadsheets_GP'
+run_parameters['pipeline_type'] = 'gene_prioritization_pipeline'
+run_parameters['correlation_measure'] = 't_test'
+
+run_parameters['run_directory'] = dcp_src
+run_parameters['results_directory'] = kn.create_dir(dir_path=output_data_dir, dir_name='gene_prioritaization_pearson')
+
+run_parameters['SHOW_COMMAND_LINE_OUTPUT'] = SHOW_COMMAND_LINE_OUTPUT
+
+if SHOW_COMMAND_LINE_OUTPUT:
+    for p in list(run_parameters.keys()):
+        print(p, ':\t\t', run_parameters[p])
+
+test_result_df = dcp_test.test_data_cleanup(run_parameters)
+
+
+# In[ ]:
+
+#                   Data_Cleanup_Pipeline   GeneSet_Characterization
+yaml_dir = '../../../Data_Cleanup_Pipeline/data/run_files'
+yaml_file = 'TEMPLATE_data_cleanup.yml'
+run_parameters = kn.get_run_parameters(yaml_dir, yaml_file)
+
+run_parameters.pop('phenotype_full_path', None)
+run_parameters['spreadsheet_data_dir'] = '../../keg_test_tools/data/geneset_characterization/spreadsheets_GSC'
+run_parameters['pipeline_type'] = 'geneset_characterization_pipeline'
+
+run_parameters['run_directory'] = dcp_src
+run_parameters['results_directory'] = kn.create_dir(dir_path=output_data_dir, dir_name='geneset_charictarization')
+
+run_parameters['SHOW_COMMAND_LINE_OUTPUT'] = SHOW_COMMAND_LINE_OUTPUT
+
+if SHOW_COMMAND_LINE_OUTPUT:
+    for p in list(run_parameters.keys()):
+        print(p, ':\t\t', run_parameters[p])
+
+test_result_df = dcp_test.test_data_cleanup(run_parameters)
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
