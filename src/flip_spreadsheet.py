47001ffc08beedd5e494d6acdd778ffc1b53b786
Daniel Lanier <lanier4@mntl-dhcp108.mntl.illinois.edu>
2017-04-18 09:29:29 -0500

    catch up research trail

--- src/flip_spreadsheet.py
+++ src/flip_spreadsheet.py
@@ -4,12 +4,22 @@ import pandas as pd
 from knpackage.toolbox import get_run_directory_and_file
 
 def flip_ss(data_directory, spreadsheet_file):
+    read_copy_transpose_write_df(data_directory, spreadsheet_file)
+    #data_directory, spreadsheet_file = get_run_directory_and_file(sys.argv)
+    # full_file_name = os.path.join(data_directory, spreadsheet_file)
+    # spreadsheet_df = pd.read_csv(full_file_name, sep='\t', index_col=0, header=0)
+    # spreadsheet_df = spreadsheet_df.transpose()
+    # spreadsheet_df.to_csv(full_file_name, sep='\t', index=True, header=True)
+    
+def read_copy_transpose_write_df(data_directory, spreadsheet_file):
     #data_directory, spreadsheet_file = get_run_directory_and_file(sys.argv)
     full_file_name = os.path.join(data_directory, spreadsheet_file)
     spreadsheet_df = pd.read_csv(full_file_name, sep='\t', index_col=0, header=0)
     spreadsheet_df = spreadsheet_df.transpose()
-    spreadsheet_df.to_csv(full_file_name, sep='\t', index=True, header=True)
+    output_file_name = os.path.join(data_directory, spreadsheet_file[0:-4] + '_T.tsv')
+    spreadsheet_df.to_csv(output_file_name, sep='\t', index=True, header=True)
+    # return output_file_name
 
 if __name__ == "__main__":
     data_directory, spreadsheet_file = get_run_directory_and_file(sys.argv)
-    flip_ss(data_directory, spreadsheet_file)
+    read_copy_transpose_write_df(data_directory, spreadsheet_file)
