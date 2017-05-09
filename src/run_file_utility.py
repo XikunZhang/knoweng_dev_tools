3c7f74fb3e3d8d2fd28a05aa21c3f2d803500bce
Daniel Lanier <lanier4@mntl-dhcp108.mntl.illinois.edu>
2017-05-09 11:17:38 -0500

    add makefile options dictionary set

--- src/run_file_utility.py
+++ src/run_file_utility.py
@@ -7,6 +7,24 @@ import pandas as pd
 import yaml
 
 
+Data_Cleanup_methods_dictionary =               {
+    'run_data_cleaning'                         : 'data_cleanup.yml',
+    'run_pasted_gene_list'                      : 'pasted_gene_cleanup.yml',
+    'run_data_cleaning_small'                   : 'TEST_1_data_cleanup.yml',
+    'run_samples_clustering_pipeline'           : 'TEST_1_samples_clustering_pipeline.yml',
+    'run_gene_prioritization_pipeline_pearson'  : 'TEST_1_gene_prioritization_pipeline_pearson.yml',
+    'run_gene_prioritization_pipeline_t_test'   : 'TEST_1_gene_prioritization_pipeline_t_test.yml',
+    'run_geneset_characterization_pipeline'     : 'TEST_1_geneset_characterization_pipeline.yml' }
+
+Samples_Clustering_methods_dictionary = {
+    'run_nmf'                           : 'BENCHMARK_1_SC_nmf.yml',
+    'run_net_nmf'                       : 'BENCHMARK_2_SC_net_nmf.yml',
+    'run_cc_nmf_serial'                 : 'BENCHMARK_3_SC_cc_nmf_serial.yml',
+    'run_cc_nmf_parallel_shared'        : 'BENCHMARK_4_SC_cc_nmf_parallel_shared.yml',
+    'run_cc_net_nmf_serial'             : 'BENCHMARK_6_SC_cc_net_nmf_serial.yml',
+    'run_cc_net_nmf_parallel_shared'    : 'BENCHMARK_7_SC_cc_net_nmf_parallel_shared.yml' }
+
+
 def update_run_file_post_clean(active_run_file_name):
     """ Update run file after data cleaning """
     STATUS = False
@@ -53,7 +71,7 @@ def set_run_file_path_to_abs(run_file_name, run_dir):
         
         null_dir, out_file_name = os.path.split(run_file_name)
         out_file_name, null_ext = os.path.splitext(out_file_name)
-        out_file_name = os.path.join(run_dir, out_file_name + '_Active.yml')
+        out_file_name = os.path.abspath( os.path.join( run_dir, out_file_name + '_Active.yml' ) )
         with open(out_file_name, 'w') as outfile:
             yaml.dump(run_parameters, outfile, default_flow_style=False)
 
